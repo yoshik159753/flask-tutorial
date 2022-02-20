2878b90b7ccb06eae09b72df508cf9db60184044
Yoshi K <kai.outsider.777+git@gmail.com>
2022-02-20 23:31:35 +0900

    import の一括フォーマット

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,6 +1,5 @@
-import psycopg2
-
 import click
+import psycopg2
 from flask import current_app, g
 from flask.cli import with_appcontext
 
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,6 @@
 import pytest
 from flask import g, session
+
 from flaskr.db import get_db
 
 
